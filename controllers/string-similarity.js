ddf0535b349751409801888da47c13e512b354ab
rushelmac <rushishelke633@gmail.com>
2020-12-28 22:29:26 +0530

    Read file using fs

--- controllers/string-similarity.js
+++ controllers/string-similarity.js
@@ -21,21 +21,24 @@ const get_similarity = function (){
     // const string1 = "Javascript/Nodejs sentence similarity. Although this was designed for sentences, it will work for other sequences as long as the user provides a scoring function for for comparing 2 elements of the sequence.";
     // const string2 = "Javascript/Nodejs sentence similarity. Although this was designed for sentences, it will work for other sequences as long as the user provides a scoring function for for comparing 2 elements of the sequence.";
     
-    const string1 = fs.readFileSync('../documents/testass1.txt', 'utf8');
-    const string2 = fs.readFileSync('../documents/testass2.txt', 'utf8');
-
-    const s1 = string1.split(" ");
-    const s2 = string2.split(" ");
-
-    console.log(string1);
-    console.log(string2);
+    fs.readFile('../documents/testass1.txt', 'utf8', function(err, string1) {
+        if (err) throw err;
+        fs.readFile('../documents/testass2.txt', 'utf8', function(err, string2){
+            if(err) console.log(err);
+            const s1 = string1.split(" ");
+            const s2 = string2.split(" ");
     
-    const winkOpts = { f: similarityScore.winklerMetaphone, options : {threshold: 0.3} };
-    // let opts = { f: someFunc, options : {threshold: 0.1} }
-    
-    console.log(similarity(s1,s2,winkOpts));
-    const res =  similarity(s1,s2,winkOpts);
-    return (res.score, res.order, res.size);
-}
+            console.log(string1);
+            console.log(string2);
+            
+            const winkOpts = { f: similarityScore.winklerMetaphone, options : {threshold: 0.3} };
+            // let opts = { f: someFunc, options : {threshold: 0.1} }
+            
+            console.log(similarity(s1,s2,winkOpts));
+            const res =  similarity(s1,s2,winkOpts);
+            return (res.score, res.order, res.size);
+        });
+    });
+};
 
 module.exports = get_similarity;
